d8c4d8d9af45e6c85c3c3b3f140da3bf65ccd3e7
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-10 21:35:20 +0900

    refactor: migrate from pyproject.toml to setup.py
    Stop using Poetry for package management.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="glyph_forge",
+    version="1.0.0",
+    description="generate character art by many character",
+    author='kishimin',
+    author_email="r8a1wu2@gmail.com",
+    url="https://github.com/kishimin/glyph-forge",
+)
