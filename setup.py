ccc9e14e855a51933f53c6dd92cf26bdbd4c6b4d
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-10 22:15:00 +0900

    style: format setup.py with Black
    Apply consistent code formatting.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="glyph_forge",
     version="1.0.0",
     description="generate character art by many character",
-    author='kishimin',
+    author="kishimin",
     author_email="r8a1wu2@gmail.com",
     url="https://github.com/kishimin/glyph-forge",
 )
