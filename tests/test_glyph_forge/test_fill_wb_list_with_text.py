98766bfc5f039299f054fb380c9aaff7c6090b5b
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-15 06:29:44 +0900

    feat: Add a feature to fill the blacklist and whiltelist with text
    This is done to repeatedly retrieve and output the specified text.

--- tests/test_glyph_forge/test_fill_wb_list_with_text.py
+++ tests/test_glyph_forge/test_fill_wb_list_with_text.py
@@ -1,10 +1,12 @@
 from glyph_forge.convert_text_to_image import str_2_img
 from glyph_forge.convert_image_to_01_list import (gray_list_2_wb_list, img_2_gray_list)
+from glyph_forge.fill_wb_list_with_text import wb_list_2_wb_char_list
 from test_glyph_forge.output import print_2D_num_list
 
-def test_can_print_the_given_list_of_2D_numbers():
+def test_can_fill_wb_list_with_text():
     img = str_2_img("般若波羅蜜多", 6, 1, 20)
     gray_list = img_2_gray_list(img)
     wb_list = gray_list_2_wb_list(gray_list)
 
-    print_2D_num_list(wb_list)
+    wb_char_list = wb_list_2_wb_char_list(wb_list, "般若波羅蜜多", "　")
+    print_2D_num_list(wb_char_list)
