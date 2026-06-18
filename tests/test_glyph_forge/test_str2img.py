0c62d3151d0bb7f0eedaa96bffbdc8e056e92cd5
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-18 22:31:38 +0900

    refactor:change char and str to text
    To ensure the names reflect their intended purpose.

--- tests/test_glyph_forge/test_str2img.py
+++ tests/test_glyph_forge/test_str2img.py
@@ -1,5 +1,4 @@
 from glyph_forge.convert_text_to_image import text_2_img
-import matplotlib.pyplot as plt
 
 def test_can_be_created_from_text_to_images():
     img = text_2_img(input_text="勝利友情努力", horizontal_len=2, vertical_len=3, text_size=50)
