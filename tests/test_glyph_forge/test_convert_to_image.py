d8493729c58333cb462eb6f870488c15f3ec4ece
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-06-18 21:37:14 +0900

    refactor: change the png output destination
    To prevent the output from being pushed.

--- tests/test_glyph_forge/test_convert_to_image.py
+++ tests/test_glyph_forge/test_convert_to_image.py
@@ -1,9 +1,6 @@
 from glyph_forge.convert_to_image import str_2_str_img
-import IPython
 
 def test_can_save_as_an_image():
     img = str_2_str_img("カニ", "エビ", " ", 2, 1, 20, 15)
 
-    img.save("ebikani.png")
-
-    IPython.display.Image("ebikani.png")
+    img.save("../output/ebikani.png")
