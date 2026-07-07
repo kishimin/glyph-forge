f02be0d911081a1826a4166b564ed2001ed3fadd
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-08 07:22:19 +0900

    feat: Creating a Fast API
    To enable sending images created with Pillow

--- src/glyph_forge/services/convert_text_to_image.py
+++ src/glyph_forge/services/convert_text_to_image.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 from pathlib import Path
+from importlib.resources import files
 
 def text_2_img(input_text: str, horizontal_len: int, vertical_len: int, text_size: int) -> Image.Image:
     """Convert the given string into an image
@@ -16,9 +17,9 @@ def text_2_img(input_text: str, horizontal_len: int, vertical_len: int, text_siz
     img = Image.new("RGBA", (text_size * horizontal_len, text_size * vertical_len) , "white")
     draw = ImageDraw.Draw(img)
 
-    font_path = Path(__file__).parent / "fonts" / "ipaexg.ttf"
-    font = ImageFont.truetype(font=font_path, size=text_size)
-
+    font_path = files("glyph_forge.fonts").joinpath("ipaexg.ttf")
+    font = ImageFont.truetype(str(font_path), size=text_size)
+    
     # Draw characters one by one
     horizontal_count = 0
     vertical_count = 0
