c325aa2e0d7ffb2859c732a6810284ff7a78b765
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-08 22:22:21 +0900

    fix: Send an empty message. Convert it to bytes and save it via streaming.
    Because it was pointed out in a review

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, responses
 from pydantic import BaseModel
 from glyph_forge.services.convert_to_image import text_2_text_img
-import secrets
-
+from io import BytesIO
 class GenerateImageRequest(BaseModel):
     frame_text: str
     inner_text: str
@@ -18,9 +17,13 @@ app = FastAPI()
 
 @app.post("/images")
 def generate_image(generateImageRequest: GenerateImageRequest):
+    if not generateImageRequest.inner_text:
+        raise ValueError("Enter a non-empty value for inner_text")
+    if not generateImageRequest.outer_text:
+        raise ValueError("Enter a non-empty value for outer_text")
     img = text_2_text_img(flame_text=generateImageRequest.frame_text, inner_text=generateImageRequest.inner_text, outer_text=generateImageRequest.outer_text,
-                          horizontal_len=generateImageRequest.frame_columns, vertical_len=generateImageRequest.frame_rows, 
-                          text_size=generateImageRequest.frame_font_size, final_text_size=generateImageRequest.output_font_size)
-    file_path = "output/" + secrets.token_urlsafe(16) + ".png"
-    img.save(file_path)
-    return responses.FileResponse(path=file_path, media_type="image/png")
+                        horizontal_len=generateImageRequest.frame_columns, vertical_len=generateImageRequest.frame_rows, 
+                        text_size=generateImageRequest.frame_font_size, final_text_size=generateImageRequest.output_font_size)
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    return responses.StreamingResponse(buffer, media_type="image/png")
